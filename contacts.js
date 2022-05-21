31cc03fedd0c034e44425d4b48ce994511f4e52c
ITigor1985 <xromer85@gmail.com>
2022-05-21 18:49:56 +0300

    new function

--- contacts.js
+++ contacts.js
@@ -9,8 +9,21 @@ async function listContacts(){
     return contacts
 }
 
-
+const getContactById = async (id) =>{
+    const contacts =await listContacts();    
+    const result = contacts.find(contact => contact.id === id);    
+    return result;
+    
+}
+async function removeContactById(id){
+    const data =await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data); 
+    const filterContacts = contacts.filter(contact => contact.id !== id);    
+    return filterContacts;
+}
 
 module.exports ={
-    listContacts
+    listContacts,
+    getContactById,
+    removeContactById
 }
--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const contactsOperation = require('./contacts')
 
 //contactsOperation.listContacts();
-const invokeAction =async ({action, id, data}) => {
+const invokeAction = async ({action, id, data}) => {
     switch(action){
         case"getAll":
-        const data = await contactsOperation.listContacts(); 
-        console.log(data);
-        return data;
+        const contacts = await contactsOperation.listContacts();
+        console.log(contacts); 
         break;
+        case"removeContact":
+        const filterContacts = await contactsOperation.removeContactById(id);  
+        console.log(filterContacts); 
+        break;        
+        case"getContact":
+        const getContact = await contactsOperation.getContactById(id); 
+        console.log(getContact);
+        break;
+        default:console.log("Error")
     }
     
 }
-invokeAction({action:"getAll"});
+//invokeAction({action:"getAll"});
+//invokeAction({action:"removeContact", id: "1"});
+//invokeAction({action:"getContact", id: "1"});
  
