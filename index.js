1d009e5acb6b5cf2ccb790b380e8e702bfbf38dc
ITigor1985 <xromer85@gmail.com>
2022-05-21 15:48:24 +0300

    create function listContacts

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const {admins} =require('./users')
-console.log(admins)
+const contactsOperation = require('./contacts')
+
+//contactsOperation.listContacts();
+const invokeAction =async ({action, id, data}) => {
+    switch(action){
+        case"getAll":
+        const data = await contactsOperation.listContacts(); 
+        console.log(data);
+        return data;
+        break;
+    }
+    
+}
+invokeAction({action:"getAll"});
+ 
--- users.js
+++
@@ -1,8 +0,0 @@
-const admins = ["vova", "kola", "shiha"]
-const clients = ["ziza", "gugga", "giga"]
-
-const  users = {
-    admins, clients
-};
-
-module.exports = users
